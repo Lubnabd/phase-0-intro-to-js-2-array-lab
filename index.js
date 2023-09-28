878513c4855202ca4844af82daad7fce5658eb6c
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-09-28 10:03:47 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+
+describe("My Cat Tests", function () {
+    let cats ;
+  
+    beforeEach(function () {
+        cats = [];
+      cats.length = 0;
+      cats.push("Milo", "Otis", "Garfield");
+    });
+
+})
